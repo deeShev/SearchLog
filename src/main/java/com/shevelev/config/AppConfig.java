bb8b8cc72455d4ba65f755730ebc644a8f8279ff
Denis <shevdionis@gmail.com>
2018-08-05 02:34:51 +0300

    refrefactoring project and add new rest controller(/search)

---
+++ src/main/java/com/shevelev/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.shevelev.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.shevelev")
+public class AppConfig {
+}
